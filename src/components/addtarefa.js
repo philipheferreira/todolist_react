29d43fd52faf659dbde3f32311d377514fba4a23
philipheferreira <philipheferreira@gmail.com>
2023-10-28 02:48:24 -0300

    Update: adicionar metodo

--- src/components/addtarefa.js
+++ src/components/addtarefa.js
@@ -1,17 +1,27 @@
 import React from "react";
+import { useState } from "react"
 
 const AddForm = () => {
+
+    const [valor, setValor] = useState("");
+    const [categoria, setCategoria] = useState("");
+
+    const mandarSubmicao = (e) => {
+        e.preventDefault();
+        console.log(valor, categoria)
+    }
+
     return <div className="add-form">
         <h2>Criar Tarefa:</h2>
-        <form>
-            <input type="text" placeholder="Digite o Título" />
-            <select>
-                <option value="" >Selecione uma categoria</option>
-                <option value="" >Trabalho</option>
-                <option value="" >Pessoal</option>
-                <option value="" >Selecione uma categoria</option>
+        <form onSubmit = {mandarSubmicao}>
+            <input type="text" placeholder="Digite o Título" onChange={(e) => setValor(e.target.valor)} />
+            <select onChange={(e) => setCategoria(e.target.valor)}>
+                <option valor="" >Selecione uma categoria</option>
+                <option valor="Trabalho" >Trabalho</option>
+                <option valor="Pessoal" >Pessoal</option>
+                <option valor="Estudos" >Selecione uma categoria</option>
             </select>
-            <button type="submite">Criar Tarefa</button>
+            <button type="submit">Criar Tarefa</button>
         </form>
     </div>
 }
